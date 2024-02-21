1598da356167bb1e6226536e06dc0fff6d091633
LarsonRagnar <larionov1992@gmail.com>
2024-02-21 09:05:54 +0300

    nedodelki

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
+import domian.Product;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Product product= new Product(
+            1,"lays",132.55,13
+        );
+        System.out.println(product);
+        
     }
 }
---
+++ src/services/VendingMachine.java
@@ -0,0 +1,55 @@
+package services;
+
+import java.util.List;
+
+import domian.Product;
+
+public class VendingMachine {
+    private Holder holder;
+    private Display display;
+    private CoinDespenser coinDespenser;
+    private List<Product> products;
+    
+    public VendingMachine(Holder holder, Display display, CoinDespenser coinDespenser, List<Product> products) {
+        this.holder = holder;
+        this.display = display;
+        this.coinDespenser = coinDespenser;
+        this.products = products;
+    }
+
+    public Holder getHolder() {
+        return holder;
+    }
+
+    public void setHolder(Holder holder) {
+        this.holder = holder;
+    }
+
+    public Display getDisplay() {
+        return display;
+    }
+
+    public void setDisplay(Display display) {
+        this.display = display;
+    }
+
+    public CoinDespenser getCoinDespenser() {
+        return coinDespenser;
+    }
+
+    public void setCoinDespenser(CoinDespenser coinDespenser) {
+        this.coinDespenser = coinDespenser;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+    public void addProduct(Product product){
+        this.products.add(product);
+    }
+    
+}
